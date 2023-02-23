4b816582248e6f54f0e08b035ea6d04819fd5c22
amandesai01 <amandesai01@gmail.com>
2023-02-23 14:24:30 +0530

    lex: logging added

--- lib/lex/_base.py
+++ lib/lex/_base.py
@@ -8,10 +8,13 @@ from ._tokenizer import tokenize
 
 
 def get_tokens_from_pdf(pdf: PDF):
+    print("Parsing PDF...")
     tokens: list[Token] = []
     pages: list[Page] = get_pages(pdf)
     for sr_no, page in enumerate(pages):
         page_no = sr_no + 1  # page_no = 1-based indexing, sr_no = 0-based indexing
+        print("Processing Page No " + str(page_no) + "...")
         lines: list[str] = get_lines(page)
         tokens.extend(tokenize(lines, page_no))
+    print("Found " + str(len(tokens)) + " Tokens...")
     return tokens
