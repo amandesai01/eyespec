3d07df8dc2dbce07112511fdb0540d353d714c17
amandesai01 <amandesai01@gmail.com>
2023-02-23 16:56:49 +0530

    cleanup and ARCHIVAL of dead code

--- lib/main.py
+++ lib/main.py
@@ -1,15 +1,24 @@
 from pathlib import Path
 
-from lib.lex import get_tokens_from_pdf
-from lib.pdf import load
-from lib.parser import create_parse_tree
+from openpyxl.workbook import Workbook
 
+from lib.jobs.job001 import SaveDivisionWiseTokenInformation
+from lib.xlsx import XlsxWriter
 
-path_to_pdf = Path('../test-case-1.pdf')
+path_to_pdf = Path('../test-case-2.pdf')
 
-pdf = load(path_to_pdf)
-tokens = get_tokens_from_pdf(pdf)
+# pdf = load(path_to_pdf)
+# tokens = get_tokens_from_pdf(pdf)
+#
+# parse_tree = create_parse_tree(tokens)
+# division_wise_tokens = exporters.group_by_division(parse_tree)
+#
+# print(division_wise_tokens)
+wb = Workbook()
+xlw = XlsxWriter(wb)
+job = SaveDivisionWiseTokenInformation(
+    pdf_file_location=path_to_pdf,
+    xlsx_writer=xlw,
+)
 
-parse_tree = create_parse_tree(tokens)
-
-print(parse_tree)
+job.run()
